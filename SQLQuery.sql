be814860fbd56ad57c195ae420904b220dcaad43
Mr Danh <danhnguyen01112@gmail.com>
2024-11-18 15:27:05 +0700

    update part 1

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -2,10 +2,10 @@
 -- 1. cài đặt bảng
 -- KhachHang
 create table khach_hang(
-	CCCD varchar(12) not null, 
+	CCCD char(12) not null, 
 	HoTen nvarchar(100) not null,
-	SDT char(5) not null,
-	Email nvarchar(100) not null,
+	SDT char(10) not null,
+	Email varchar(100) not null,
 	Phai nvarchar(3)
 
 	primary key(CCCD)
@@ -14,12 +14,12 @@ create table khach_hang(
 -- the
 create table the(
 	MaThe char(5) not null,
-	CCCD varchar(12) not null,
+	CCCD char(12) not null,
 	NgayLap datetime not null,
 	LoaiThe varchar(10) not null,
-	TieuDung float,
+	TieuDung float(24),
 	NhanVienLap char(5) not null,
-	CapNhat nvarchar(100),
+	CapNhat datetime,
 
 	primary key(MaThe)
 )
@@ -37,8 +37,8 @@ create table chuong_trinh(
 create table giam_gia(
 	MaChuongTrinh char(5) not null,
 	LoaiThe varchar(10) not null,
-	UuDaiChietKhau float,
-	GiamGia float,
+	UuDaiChietKhau float(24),
+	GiamGia float(24),
 	TangSP char(5),
 
 	primary key(MaChuongTrinh, LoaiThe)
@@ -56,28 +56,28 @@ create table tham_gia_chuong_trinh(
 -- 2. cai dat khoa ngoai
 ---- the
 --alter table the
---add constraint FK_the_khachHang
+--add constraint FK_the_khachHang_CCCD
 --foreign key (CCCD)
 --references khach_hang(CCCD)
 
 --alter table the
---add constraint FK_the_nhanVien
+--add constraint FK_the_nhanVien_NhanVienLap
 --foreign key (NhanVienLap)
 --references nhan_vien(MaNhanVien)
 
 -- giam gia
 alter table giam_gia
-add constraint FK_giamGia_chuongTrinh
+add constraint FK_giamGia_chuongTrinh_MaChuongTrinh
 foreign key (MaChuongTrinh)
 references chuong_trinh(MaChuongTrinh)
 
 -- tham gia chuong trinh
 alter table tham_gia_chuong_trinh
-add constraint FK_thamGiaChuongTrinh_chuongTrinh
+add constraint FK_thamGiaChuongTrinh_chuongTrinh_MaChuongTrinh
 foreign key (MaChuongTrinh)
 references chuong_trinh(MaChuongTrinh)
 
 --alter table tham_gia_chuong_trinh
---add constraint FK_thamGiaChuongTrinh_hoaDon
+--add constraint FK_thamGiaChuongTrinh_hoaDon_MaPhieu
 --foreign key (MaPhieu)
 --references hoa_don(MaPhieu)
