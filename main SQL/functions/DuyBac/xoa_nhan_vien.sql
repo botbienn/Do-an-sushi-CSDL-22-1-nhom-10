3f743eea56af3a04c1adb4c80e9554cdba187ca9
botbienn <vubac2004@gmail.com>
2024-12-12 18:05:08 +0700

    updated xoa nhan vien

--- main SQL/functions/DuyBac/xoa_nhan_vien.sql
+++ main SQL/functions/DuyBac/xoa_nhan_vien.sql
@@ -1,13 +1,4 @@
-CREATE OR ALTER PROCEDURE xoa_nhan_vien_v01
-    @MaNV CHAR(5) 
-AS
-BEGIN
-    DELETE FROM nhan_vien 
-    WHERE MaNV = @MaNV
-END
-go
-
-CREATE OR ALTER PROCEDURE xoa_nhan_vien_v02
+CREATE OR ALTER PROCEDURE xoa_nhan_vien
     @MaNV CHAR(5) 
 AS
 BEGIN
