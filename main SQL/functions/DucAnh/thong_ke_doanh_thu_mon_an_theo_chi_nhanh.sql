1a273f68b1a1dea0f62f4d9263cdb35e0eaa4af9
DucEngg <ducanhph2@gmail.com>
2024-12-14 14:24:54 +0700

    update proc

---
+++ main SQL/functions/DucAnh/thong_ke_doanh_thu_mon_an_theo_chi_nhanh.sql
@@ -0,0 +1,27 @@
+﻿--thống kê doanh thu theo món ăn của từng chi nhánh
+GO
+CREATE OR ALTER PROCEDURE ThongKeDoanhThuMonAn
+AS
+BEGIN
+    SELECT 
+        cn.MaCN AS MaChiNhanh,
+        cn.TenCN AS TenChiNhanh,
+        ma.MaMon AS MaMon,
+        ma.TenMon AS TenMon,
+        SUM(mmpd.SoLuong) AS SoLuong,
+        SUM(mmpd.SoLuong * ma.Gia) AS DoanhThu
+    FROM 
+        chi_nhanh cn
+    JOIN 
+        phieu_dat pd ON cn.MaCN = pd.MaCN
+    JOIN 
+        ma_mon_phieu_dat mmpd ON pd.MaPhieu = mmpd.MaPhieu
+    JOIN 
+        mon_an ma ON mmpd.MaMon = ma.MaMon
+    GROUP BY 
+        cn.MaCN, cn.TenCN, ma.MaMon, ma.TenMon
+    ORDER BY 
+        cn.MaCN, ma.MaMon;
+END;
+GO
+
---
+++ main SQL/functions/DucAnh/tim_kiem_hoa_don_theo_ngay.sql
@@ -0,0 +1,35 @@
+﻿--Tìm kiếm hóa đơn theo ngày
+CREATE OR ALTER PROCEDURE TimKiemHoaDonTheoNgay
+    @StartDate DATE,     
+    @EndDate DATE          
+AS
+BEGIN
+    BEGIN TRY
+        SELECT 
+            hd.MaPhieu AS MaHoaDon,
+            hd.TongTien,
+            hd.GiamGia,
+            hd.ThanhTien,
+            hd.DiemTichLuy,
+            pd.NgayDat,
+            pd.MaCN AS MaChiNhanh,
+            pd.LoaiPhieu,
+            pd.CCCD AS MaKhachHang
+        FROM 
+            hoa_don hd
+        INNER JOIN 
+            phieu_dat pd ON hd.MaPhieu = pd.MaPhieu
+        WHERE 
+            pd.NgayDat BETWEEN @StartDate AND @EndDate
+        ORDER BY 
+            pd.NgayDat DESC;
+
+        PRINT 'Tim kiem hoa don theo ngay thanh cong.';
+    END TRY
+    BEGIN CATCH
+        PRINT 'Da xay ra loi: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+GO
+
+
